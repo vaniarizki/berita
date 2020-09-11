74489bb0a78c9fbe037ed10fd00feb2b0c3d137d
vaniarizki <68143356+vaniarizki@users.noreply.github.com>
2020-09-11 18:20:38 +0700

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,7 +1,8 @@
 const RSS_URL = [
   "https://www.antaranews.com/rss/ekonomi",
   "https://www.cnnindonesia.com/nasional/rss",
-  "http://rss.detik.com/index.php/detikcom_nasional"
+  "http://rss.detik.com/index.php/detikcom_nasional",
+  "https://www.vice.com/id_id/rss"
 ]
 const feedArea = document.getElementById("feedArea")
 
